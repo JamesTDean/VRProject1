494424fffccb5408312fab03a1d9fe4ddfde08fa
JamesTDean <jtdean52@gmail.com>
2024-02-23 20:26:55 -0600

    Added Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private GameObject world;
+    private Transform cameraRig;
     private Rigidbody rb;
     private float speed = 3f;
     private float forceMagnitude = 5f;
@@ -12,20 +13,27 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
         world = GameObject.Find("World");
+        cameraRig = transform.Find("OVRCameraRig");
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //move character when key press is detected
         if (Input.GetKey("up"))
         {
-            rb.velocity = gameObject.transform.forward * speed;
+            rb.velocity = cameraRig.forward * speed;
         }
 
+        //VR move character on joystick touch
+
+        //find direction to center of world
         Vector3 toCenterDir = (world.transform.position - gameObject.transform.position).normalized;
         Vector3 toBodyDir = (gameObject.transform.position - world.transform.position).normalized;
+        
+        //add false gravity so that character stays on surface
         rb.AddForce(forceMagnitude * toCenterDir);
 
         // update the objects rotation in relation to the planet
